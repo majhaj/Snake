6847dfffd718d5fcfba86cbf1d7ecca1ab10e19b
majhaj <maja.hajda@gmail.com>
2025-05-13 23:50:34 +0200

    Reapply "Adding game base"
    This reverts commit fb2246b90e1b45f393b36b55cb25e31567db0dcf.

--- Snake/Game.cs
+++ Snake/Game.cs
@@ -8,5 +8,33 @@ namespace Snake
 {
     public class Game
     {
+        private Snake _snake;
+        private Food _food;
+        private bool _isRunning;
+
+        public void Start()
+        {
+            Console.Clear();
+            _snake = new Snake();
+            _food = new Food();
+            // Generate food
+
+            _isRunning = true;
+
+            while (_isRunning)
+            {
+                // Update game state
+                // Check for collisions
+                // Render game
+                // Handle input
+            }
+        }
+
+        private void Draw()
+        {
+            Console.Clear();
+            // Draw snake
+            // Draw food
+        }
     }
 }
