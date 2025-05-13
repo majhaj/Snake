0ab303d466abade5920e55554f51e60490034b94
majhaj <maja.hajda@gmail.com>
2025-05-14 00:06:22 +0200

    some changes

--- Snake/Game.cs
+++ Snake/Game.cs
@@ -8,5 +8,52 @@ namespace Snake
 {
     public class Game
     {
+        private Snake snake;
+        private Food food;
+        private bool isRunning;
+
+        public void Start()
+        {
+            Console.Clear();
+            snake = new Snake();
+            food = new Food();
+            food.Generate(snake.Body);
+
+            isRunning = true;
+
+            while (isRunning)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    snake.ChangeDirection(key);
+                }
+
+                snake.Move();
+
+                if (snake.CheckCollision())
+                {
+                    isRunning = false;
+                    Console.SetCursorPosition(0, 20);
+                    Console.WriteLine("Game Over!");
+                    break;
+                }
+
+                if (snake.Eat(food.Position))
+                {
+                    food.Generate(snake.Body);
+                }
+
+                Draw();
+                Thread.Sleep(100);
+            }
+        }
+
+        private void Draw()
+        {
+            Console.Clear();
+            snake.Draw();
+            food.Draw();
+        }
     }
 }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -8,5 +8,64 @@ namespace Snake
 {
     public class Snake
     {
+        public List<(int x, int y)> Body { get; private set; }
+        private (int x, int y) direction;
+
+        public Snake()
+        {
+            Body = new List<(int x, int y)> { (10, 10) };
+            direction = (1, 0);
+        }
+
+        public void ChangeDirection(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.UpArrow: if (direction.y != 1) direction = (0, -1); break;
+                case ConsoleKey.DownArrow: if (direction.y != -1) direction = (0, 1); break;
+                case ConsoleKey.LeftArrow: if (direction.x != 1) direction = (-1, 0); break;
+                case ConsoleKey.RightArrow: if (direction.x != -1) direction = (1, 0); break;
+            }
+        }
+
+        public void Move()
+        {
+            var head = Body[0];
+            var newHead = (head.x + direction.x, head.y + direction.y);
+            Body.Insert(0, newHead);
+            Body.RemoveAt(Body.Count - 1);
+        }
+
+        public bool Eat((int x, int y) foodPosition)
+        {
+            if (Body[0] == foodPosition)
+            {
+                Body.Add(Body[^1]);
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckCollision()
+        {
+            var head = Body[0];
+            if (head.x < 0 || head.y < 0 || head.x >= Console.WindowWidth || head.y >= Console.WindowHeight)
+                return true;
+
+            for (int i = 1; i < Body.Count; i++)
+                if (Body[i] == head) return true;
+
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach (var part in Body)
+            {
+                Console.SetCursorPosition(part.x, part.y);
+                Console.Write("O");
+            }
+        }
     }
 }
+
